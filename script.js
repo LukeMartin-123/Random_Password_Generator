c6318cbef9671d92291fcfba28fd68f521d1cd77
Luke Martin <ljmart02@gmail.com>
2020-11-28 15:01:20 -0700

    Updated javascript with tips from class

--- script.js
+++ script.js
@@ -10,5 +10,60 @@ function writePassword() {
 
 }
 
+var allUpper = [ ]
+var allLower = [ ]
+var allNumbers = [ ]
+var allSpecial = [ ]
+
+
+function generatePassword() {
+  // Randomly genrate the password here
+
+  var allChoices = []
+  var newPassword = "";
+
+  // What is the length of your password?
+  var passwordLength = prompt("What is your password's length? (Must be between 8 and 128 characters"); 
+
+  if(passwordLength >= 8 && <= 128) {
+  
+    var useUpper = confirm("Do you want uppercase characters?");
+
+    var useLower = confirm("Do you want lowercase characters?");
+
+    var useNumbers = confirm("Do you want numbers?");
+
+    var useSpecial = confirm("Do you want special characters?");
+
+    if(useUpper === true) {
+      allChoices.concat(allUpper);
+    }
+
+    if(useLower === true) {
+      allChoices.concat(allLower);
+    }
+
+    if(useNumbers === true) {
+      allChoices.concat(allNumbers);
+    }
+
+    if(useSpecial === true) {
+      allChoices.concat(allSpecial);
+    }
+  
+    for(var 1 = 1; 1 >= passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * allChoices.length);
+
+    }
+  
+
+  return newPassword
+
+ } else {
+      alert("Error! Length must be between 8 and 128 chracters")
+ }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
